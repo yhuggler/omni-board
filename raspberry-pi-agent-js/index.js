aa62b56ec7a849383db4f4b1b504aaeef6a25f0d
Yannick Huggler <yannick.huggler@bluewin.ch>
2019-06-30 22:20:52 +0200

    strange bug with raspi client

--- raspberry-pi-agent-js/index.js
+++ raspberry-pi-agent-js/index.js
@@ -3,14 +3,29 @@ require('dotenv').config();
 
 const si = require('systeminformation');
 
+let cpuReading = {
+    "currentLoad": 0,
+    "currentClockspeed": 0,
+    "maxClockspeed": 0,
+    "minClockspeed": 0,
+    "currentTemp": 0,
+    "tempLimitTdp": 0,
+    "serverIdFk": 2
+};
+
 
 // Retrieve System Vitals
+/*
 si.cpu()
     .then(data => console.log(data))
     .catch(error => console.log(error));
-
+    */
 si.currentLoad()
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        cpuReading.currentLoad = data['currentload'];
+        console.log(cpuReading);
+    })
     .catch(error => console.log(error));
 
 
