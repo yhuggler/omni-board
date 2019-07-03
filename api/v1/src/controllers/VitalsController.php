37fd23de94307087a36efb5822431344ebead56e
Yannick Huggler <yannick.huggler@bluewin.ch>
2019-07-03 10:19:56 +0200

    Added MySQL Setup Script for the Schema

--- api/v1/src/controllers/VitalsController.php
+++ api/v1/src/controllers/VitalsController.php
@@ -16,13 +16,11 @@ class VitalsController {
         $createdAt = time();        
         $serverId = $request['serverId'];
 
-        // First of all, I have to handle the recieval of the data.
         $mapper = new JsonMapper();
 
         $cpuReadingData = $inputs['cpuReading'];
         $systemStatsData = $inputs['systemStats'];
         
-        // Mapping the json data to the custom php classes.
         $cpuReading = $mapper->map($cpuReadingData, new CpuReading());
         $systemStats = $mapper->map($systemStatsData, new SystemStats());
 
