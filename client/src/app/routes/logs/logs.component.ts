b46c28a958821e2b0c69b8728c5b2d20c0693957
Yannick Huggler <yannick.huggler@bluewin.ch>
2019-08-07 07:39:52 +0200

    Created Logs Component

--- client/src/app/routes/logs/logs.component.ts
+++ client/src/app/routes/logs/logs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LogsService } from "../../services/logs.service";
 import { Log } from "../../models/log";
+import { MatTableDataSource } from "@angular/material";
 
 @Component({
     selector: 'app-logs',
@@ -9,26 +10,24 @@ import { Log } from "../../models/log";
 })
 export class LogsComponent implements OnInit {
 
-    public logs: Log[];
-	public displayedColumnsLogs: string[] = ['logId', 'loggingLevel', 'message', 'client', 'ipAddress', 'requestMethod', 'requestURI', 'createdAt'];
+    public logs: MatTableDataSource<Log>;
+	public displayedColumnsLogs: string[] = ['loggingLevel', 'requestMethod', 'requestURI', 'message', 'ipAddress', 'createdAt'];
 
     constructor(private logsService: LogsService) { }
 
     ngOnInit() {
-        setTimeout(() => {
-            this.refresh();
-        }, 1000);
+        this.refresh();
     }
 
     public refresh() {
-        this.logs = [];
+        const logs: Log[] = [];
 
         this.logsService.getLogs().subscribe(response => {
             if (!response['error']) {
                 for (let i = 0; i < response['logs'].length; i++) {
                     const temporaryLog = response['logs'][i];
 
-                    this.logs.push(new Log(temporaryLog['logId'],
+                    logs.push(new Log(temporaryLog['logId'],
                         temporaryLog['loggingLevel'], 
                         temporaryLog['message'], 
                         temporaryLog['client'], 
@@ -37,8 +36,14 @@ export class LogsComponent implements OnInit {
                         temporaryLog['requestURI'], 
                         temporaryLog['createdAt']));
                 }
+
+                this.logs = new MatTableDataSource<Log>(logs);
             }
         });
+    }
+
+    public getLoggingLevelStyle(loggingLevel: string) {
+        return loggingLevel.toLowerCase();
     }
 
 }
